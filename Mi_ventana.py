44daaf2a3eccc43c026d0bb6e6c94e46dcb57b9a
Pedro Luis Alcántara Acevedo <pedroluis00alcantara@gmail.com>
2026-03-11 22:15:17 -0400

    Add files via upload

---
+++ Mi_ventana.py
@@ -0,0 +1,49 @@
+# type:ignore
+import tkinter as tk
+from dotenv import load_dotenv
+import os
+from tkinter import messagebox
+
+load_dotenv()
+user_password = os.getenv("USER_PASSWORD")
+
+def capturar_valor():
+    valor_usuario = campo_entrada_usuario.get()
+    valor_password = campo_entrada_password.get()
+    print(valor_usuario, valor_password)
+
+    if  valor_usuario == "pedro" and valor_password == "1":
+        messagebox.showinfo("Acceso", "Login correcto")
+    else:
+        messagebox.showerror("Error", "Usuario o contraseña incorrectos")
+
+
+
+ventana = tk.Tk()  # Instanciando la clase Tk de Tkinter
+ventana.title("Mi primera ventana en tkinter")
+ventana.geometry("600x400")
+
+label_informacion = tk.Label(ventana, text="Bienbenido a mi pequeña ventana",border=10, foreground="#000000", font=("Arial",20))
+label_informacion.pack()
+
+# Label o texto que se muestra
+label_usuario = tk.Label(ventana, text="Usuario",border=10, foreground="#000000",font=("Arial",18, "bold"))
+label_usuario.pack(pady=10)
+
+# Campo de entrada -> input
+campo_entrada_usuario = tk.Entry(ventana, font=("Arial", 16))
+campo_entrada_usuario.pack()
+
+# Campo de entrada -> input
+label_password = tk.Label(ventana, text="Password", border=10, font=("Arial", 18, "bold"))
+label_password.pack(pady=10)
+
+campo_entrada_password = tk.Entry(ventana, show="😐", font=("Arial", 16, "bold"))
+campo_entrada_password.pack()
+
+# Botón
+boton = tk.Button(ventana, text="Iniciar",border=10 ,width=12, height=2 , command=capturar_valor)
+boton.pack(pady=20)
+
+# Bucle principal que está en escucha de eventos
+ventana.mainloop()
